be2391ca033b42e0f859b063f1417519b3127efc
x9matrix <71664759+x9matrix@users.noreply.github.com>
2025-04-02 04:16:12 +0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,4 +1,6 @@
-document.querySelectorAll('a[href^="http"]').forEach(link => {
-    link.setAttribute('target', '_blank');
-    link.setAttribute('rel', 'noopener noreferrer');
+document.addEventListener('DOMContentLoaded', function() {
+    document.querySelectorAll('a[href^="http"]').forEach(link => {
+        link.setAttribute('target', '_blank');
+        link.setAttribute('rel', 'noopener noreferrer');
+    });
 });
